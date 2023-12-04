ee01cc88446c97fcc61b5047fd0627a9d4e0ad5e
Michael Amadi <amadimichaeld@gmail.com>
2023-12-04 19:08:10 +0100

    fix _getCollateralAmountFromCurrencyValue wrong calc, typo, update protocol-core dependency, fix file wrong naming

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
 
-import {MultiStaticcall} from "../src/mutliStaticcall.sol";
+import {MultiStaticcall} from "../src/multiStaticcall.sol";
 import {VaultGetters} from "../src/vaultGetters.sol";
 import {VaultRouter} from "../src/vaultRouter.sol";
-
-import {BaseScript, stdJson, console2} from "./base.s.sol";
+import {BaseScript} from "./base.s.sol";
 
 contract DeployScript is BaseScript {
-    using stdJson for string;
-
     function run()
         external
         broadcast
--- test/vaultRouter.t.sol
+++ test/vaultRouter.t.sol
@@ -2,12 +2,18 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
+import {VaultGetters} from "../src/vaultGetters.sol";
 import {VaultRouter} from "../src/VaultRouter.sol";
+import {MultiStaticcall} from "../src/multiStaticcall.sol";
 
 contract VaultRouterTest is Test {
     VaultRouter public vaultRouter;
+    VaultGetters public vaultGetters;
+    MultiStaticcall public multiStaticcall;
 
     function setUp() public {
         vaultRouter = new VaultRouter();
+        vaultGetters = new VaultGetters();
+        multiStaticcall = new MultiStaticcall();
     }
 }
